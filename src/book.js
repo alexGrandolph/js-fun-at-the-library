68dc087798533a6bf5ccc8f94c13d5a4a1a2d5a4
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-13 20:13:34 -0600

    add half of book.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,33 @@
-function createTitle() {
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, array) {
+  if (array.includes(review)) {
+
+  } else {
+    return array.push(review)
+  }
   
+}
+
+function calculatePageCount(title) {
+  return title.length * 20
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
   // writeBook,
   // editBook
 }
